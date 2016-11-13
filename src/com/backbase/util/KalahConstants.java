f003354503b589d8ae7fa45b0d92b9b14440df30
krishnanagaraj <krishna.nagaraj@yahoo.com>
2016-11-14 01:58:36 +0530

    Code refactoring

--- src/com/backbase/util/KalahConstants.java
+++ src/com/backbase/util/KalahConstants.java
@@ -8,7 +8,7 @@ public class KalahConstants {
     public static final String PLAYER2 = "second";
 
     public static final String DRAW_GAME = "Draw game";
-    public static final String PLAYER_ENTRY = "Enter the name of the %s player";
+    public static final String PLAYER_ENTRY = "Enter name of the %s player";
 
     public static final String WHICH_PIT_TO_REMOVE = "%s, Which Pit to remove ?";
     public static final String INVALID_MOVE = "Invalid move.  Try again.";
